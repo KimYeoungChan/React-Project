47f308eeba940c99ad95a54323cb97424a900182
Channy <69968484+KimYeoungChan@users.noreply.github.com>
2023-09-21 23:52:04 +0900

    user-input-form Area

--- code/Expense Calculator/src/App.js
+++ code/Expense Calculator/src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   // 데이터
@@ -34,7 +35,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       {/* expenses 컴포넌트 items 안에 expenses데이터 넣음 */}
       <Expenses items={expenses} />
     </div>
